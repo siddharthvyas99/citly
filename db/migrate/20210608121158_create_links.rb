73b40f3b0232b6622d77e4a42cddc19b48e8b6df
Siddharth Vyas <siddharth.vyas9299@gmail.com>
2021-06-08 17:45:18 +0530

    Added migrations for links table

---
+++ db/migrate/20210608121158_create_links.rb
@@ -0,0 +1,12 @@
+class CreateLinks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :links do |t|
+      t.string :original_url, null: false, index: { unique: true }
+      t.string :shortened_url, null: false, index: { unique: true }
+      t.string :slug, null: false
+      t.boolean :is_pinned, :default => false
+      t.integer :clicked, :default => 0
+      t.timestamps
+    end
+  end
+end
